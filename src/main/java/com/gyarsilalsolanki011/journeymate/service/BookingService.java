8a8f6773a9d613b89dd74677f992299a3fed8b82
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-10-20 11:01:20 +0530

    Booking service interface defined

--- src/main/java/com/gyarsilalsolanki011/journeymate/service/BookingService.java
+++ src/main/java/com/gyarsilalsolanki011/journeymate/service/BookingService.java
@@ -1,4 +1,15 @@
 package com.gyarsilalsolanki011.journeymate.service;
 
+import com.gyarsilalsolanki011.journeymate.model.dto.BookingDTO;
+
 public class BookingService implements com.gyarsilalsolanki011.journeymate.service.Repository.BookingService {
+    @Override
+    public String bookTrip(BookingDTO bookingDTO) {
+        return null;
+    }
+
+    @Override
+    public String cancelBooking(Integer bookingId) {
+        return null;
+    }
 }
--- src/main/java/com/gyarsilalsolanki011/journeymate/service/Repository/BookingService.java
+++ src/main/java/com/gyarsilalsolanki011/journeymate/service/Repository/BookingService.java
@@ -1,4 +1,8 @@
 package com.gyarsilalsolanki011.journeymate.service.Repository;
 
+import com.gyarsilalsolanki011.journeymate.model.dto.BookingDTO;
+
 public interface BookingService {
+    String bookTrip(BookingDTO bookingDTO);
+    String cancelBooking(Integer bookingId);
 }
